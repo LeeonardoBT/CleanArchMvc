f72cb539556f7d057df29cabd9fb76c9223fc10d
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2023-06-27 22:53:55 -0300

    Camada de domain

---
+++ CleanArchMvc.Domain/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+
+        /*public DateTime CreatedDate { get; protected set; }
+        public string CreatedBy { get; protected set; }
+        public DateTime? LastModifiedDate { get; protected set; }
+        public string LastModifiedBy { get; protected set; }
+        public bool IsDeleted { get; protected set; }
+        public DateTime? DeletedDate { get; protected set; }
+        public string DeletedBy { get; protected set; }*/
+    }
+}
