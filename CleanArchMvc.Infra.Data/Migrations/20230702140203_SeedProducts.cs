e9edc494b3a18a33bdc29f7152b690fdd62a4e21
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2023-07-02 11:08:50 -0300

    Infra IOC + migrations

---
+++ CleanArchMvc.Infra.Data/Migrations/20230702140203_SeedProducts.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Caderno','Caderno espiral 100 folhas',7.45,50,'caderno1.jpg', 1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Estojo','Estojo escolar preto',5.50,30,'estojo1.jpg', 1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Borracha','Borracha branca pequena',3.00,100,'borracha1.jpg', 1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                "VALUES('Calculadora','Calculadora simples',16.99,20,'calculadora1.jpg', 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
