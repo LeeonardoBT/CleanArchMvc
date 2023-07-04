95cdabdb6fa946245eb0c8224ed58673b247235d
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2023-07-04 13:36:09 -0300

    Handlers

---
+++ CleanArchMvc.Application/Products/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchMvc.Application.Products.Queries;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductByIdHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetProductByIdAsync(request.Id);
+        }
+    }
+}
